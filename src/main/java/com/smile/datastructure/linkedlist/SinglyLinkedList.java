4fa7c18a2cf88d1fa60a146dedacf2364fa4e615
kiligsmile <kiligsmile@163.com>
2023-04-24 22:25:27 +0800

    添加结点

--- src/main/java/com/smile/datastructure/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/smile/datastructure/linkedlist/SinglyLinkedList.java
@@ -1,10 +1,32 @@
 package com.smile.datastructure.linkedlist;
 
 
-public class SinglyLinkedList {
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+//单向链表
+public class SinglyLinkedList implements Iterable<Integer> {
     private Node head=null;   // 头指针
 
-//    节点类
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            Node p=head;
+            @Override
+            public boolean hasNext() {    // 是否有下一个元素
+                return p!=null;
+            }
+
+            @Override
+            public Integer next() {   // 返回当前值，并指向下一个元素
+                int v = p.value;
+                p=p.next;
+                return v;
+            }
+        };
+    }
+
+    //    节点类
     private static class Node{
         int value;   // 值
         Node next;  // 下一个节点指针
@@ -20,11 +42,38 @@ public class SinglyLinkedList {
         head= new Node(value,head);
     }
 
-    public void loop(){
+    public void loop1(Consumer<Integer> consumer){
         Node p=head;
         while(p!=null){
-            System.out.println(p.value);
+            consumer.accept(p.value);
             p=p.next;
         }
     }
+
+    public void loop2(Consumer<Integer> consumer){
+        for (Node p=head;p!=null;p=p.next){
+            consumer.accept(p.value);
+        }
+    }
+
+    private Node findLast(){
+        if(head==null){  // 空链表
+            return null;
+        }
+        Node p;
+        for (p=head;p.next!=null;p=p.next){
+
+        }
+        return p;
+    }
+
+    public void addLast(int value){
+        Node last = findLast();
+        if(last==null){
+            addFirst(value);
+            return;
+        }
+        last.next=new Node(value,null);
+    }
+
 }
--- src/test/java/com/smile/datastructure/linkedlist/TestSinglyLinkedList.java
+++ src/test/java/com/smile/datastructure/linkedlist/TestSinglyLinkedList.java
@@ -1,7 +1,15 @@
 package com.smile.datastructure.linkedlist;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
 
 public class TestSinglyLinkedList {
 
@@ -12,6 +20,31 @@ public class TestSinglyLinkedList {
         list.addFirst(2);
         list.addFirst(3);
         list.addFirst(4);
-        list.loop();
+        list.loop2(value->{
+            System.out.println(value);
+        });
+    }
+
+    @Test
+    public void test2(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addFirst(4);
+        for (Integer value : list) {
+            System.out.println(value);
+        }
+    }
+
+    @Test
+    public void test3(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        List<Integer> intList = Arrays.asList(1,2,3,4);
+        Assertions.assertIterableEquals(intList,list);
     }
 }
