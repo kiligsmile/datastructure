8d02f68c6dbfe93dbd92057e18e7593a8b2e2d48
kiligsmile <kiligsmile@163.com>
2023-05-07 18:21:46 +0800

    二叉树

---
+++ src/main/java/com/smile/datastructure/binarytree/E08ExpressionTree.java
@@ -0,0 +1,71 @@
+package com.smile.datastructure.binarytree;
+
+import java.util.LinkedList;
+
+/**
+ * 根据后缀表达式构造表达式树
+ * 二叉树的后序遍历可以得到后缀表达式
+ */
+public class E08ExpressionTree {
+
+    static class TreeNode {
+        public String val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(String val) {
+            this.val = val;
+        }
+
+        public TreeNode(TreeNode left, String val, TreeNode right) {
+            this.left = left;
+            this.val = val;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return this.val;
+        }
+    }
+
+    /*
+        中缀表达式           (2-1)*3
+        后缀（逆波兰）表达式   21-3*
+
+        1.遇到数字入栈
+        2.遇到运算符, 出栈两次, 与当前节点建立父子关系, 当前节点入栈
+
+        栈
+        |   |
+        |   |
+        |   |
+        _____
+
+        表达式树
+            *
+           / \
+          -   3
+         / \
+        2   1
+
+        21-3*
+     */
+    public TreeNode constructExpressionTree(String[] tokens) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        for (String t : tokens) {
+            if(t=="+"||t=="-"||t=="*"||t=="/"){
+                TreeNode right = stack.pop();
+                TreeNode left = stack.pop();
+                TreeNode parent = new TreeNode(t);
+                parent.left = left;
+                parent.right = right;
+                stack.push(parent);
+            }
+            else{
+                stack.push(new TreeNode(t));
+            }
+        }
+        return stack.peek();
+    }
+}
